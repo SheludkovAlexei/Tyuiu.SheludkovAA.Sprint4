7add6e017e2505e8c1a134f924da24e30fdc29a4
Tyuiu.SheludkovAA@outlook.com <Tyuiu.SheludkovAA@outlook.com>
2023-11-09 20:17:39 +0500

    Task4 V14

---
+++ Tyuiu.SheludkovAA.Sprint4.Task4.V14.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SheludkovAA.Sprint4.Task4.V14.Lib;
+
+namespace Tyuiu.SheludkovAA.Sprint4.Task4.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] mtr = new int[5, 5] { { 1, 3, 7, 7, 6, },
+                                         { 4, 2, 6, 3, 3, },
+                                         { 4, 5, 1, 4, 6, },
+                                         { 7, 5, 2, 7, 7, },
+                                         { 2, 4, 4, 4, 7, } };
+
+            int[,] wait = new int[5, 5] { { 0, 0, 0, 0, 6, },
+                                          { 4, 2, 6, 0, 0, },
+                                          { 4, 0, 0, 4, 6, },
+                                          { 0, 0, 2, 0, 0, },
+                                          { 2, 4, 4, 4, 0, } }; ;
+            int[,] res = ds.Calculate(mtr);
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SheludkovAA.Sprint4.Task4.V14/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SheludkovAA.Sprint4.Task4.V14.Lib;
+
+namespace Tyuiu.SheludkovAA.Sprint4.Task4.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+   
+
+            Console.Title = "Спринт #3 | Выполнил: Шелудков А. А. | АСОиУб-23-1 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Шелудков А. А. | АСОиУб-23-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Количество строчек в массиве : ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Количество колонок в массиве : ");
+            int coll = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Количество колонок и строчек в массиве : ");
+            Console.WriteLine(rows + " | " + coll);
+            int[,] mtr = new int[rows, coll];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < coll; j++)
+                {
+                    Console.WriteLine($"Введите {i},{j} элемент массива");
+                    mtr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nМассив : ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < coll; j++)
+                {
+                    Console.WriteLine($"{mtr[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Измененная матрица : ");
+            int[,] mtr1 = ds.Calculate(mtr);
+            Console.WriteLine("\nМассив : ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < coll; j++)
+                {
+                    Console.WriteLine($"{mtr1[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
